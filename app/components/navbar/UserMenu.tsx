90addbeba892ab0a22ba6ec2b4c153c8766c0423
TienDuc <tienducnguyen174@gmail.com>
2023-06-10 20:54:18 +0700

    fix: login modal, authenication

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,8 +5,10 @@ import Avatar from '../Avatar';
 import { useCallback, useState } from 'react'
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
 
-const UserMenu = () =>{
+const UserMenu = () => {
+  const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
@@ -14,10 +16,10 @@ const UserMenu = () =>{
   }, []);
 
 
-  return(
+  return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <div onClick={() => {}}
+        <div onClick={() => { }}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
           Airbnb your home
@@ -60,12 +62,12 @@ const UserMenu = () =>{
           ">
           <div className="flex flex-col cursor-pointer">
             <>
-              <MenuItem 
-                onClick={() => {}}
+              <MenuItem
+                onClick={loginModal.onOpen}
                 label="Login"
               />
-              <MenuItem 
-                onClick = {registerModal.onOpen}
+              <MenuItem
+                onClick={registerModal.onOpen}
                 label="Sign up"
               />
             </>
