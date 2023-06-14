b0172c89f3c8700c6da12aa4b990fae2d8bbf0e6
TienDuc <tienducnguyen174@gmail.com>
2023-06-14 21:11:05 +0700

    update: ui component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='text-rose-500 text-2xl'>Hello AirBnb</div>
+    <div className='text-rose-500 text-2xl'>AirBnb</div>
   )
 }
