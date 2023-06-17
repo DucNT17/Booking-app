f61ad3aade94b569f4b61a6a81f9410d9ba4205c
TienDuc <tienducnguyen174@gmail.com>
2023-06-17 13:23:45 +0700

    feat: structure fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import { Nunito } from 'next/font/google';
-import Navbar from './components/navbar/Navbar';
+import { Nunito } from 'next/font/google'
+
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import RentModal from '@/app/components/modals/RentModal';
+
+import ToasterProvider from '@/app/providers/ToasterProvider';
+
 import './globals.css'
 import ClientOnly from './components/ClientOnly';
-import RegisterModal from './components/modals/RegisterModal';
-import ToasterProvider from './providers/ToasterProvider';
-import LoginModal from './components/modals/LoginModal';
-import getCurrentUser from '@/actions/getCurrentUser';
-import RentModal from './components/modals/RentModal';
+import getCurrentUser from './actions/getCurrentUser';
 
 export const metadata = {
   title: 'Airbnb',
-  description: 'Airbnb clone',
+  description: 'Airbnb Clone',
 }
 
-const font = Nunito({
-  subsets: ['latin'],
-})
+const font = Nunito({ 
+  subsets: ['latin'], 
+});
 
 export default async function RootLayout({
   children,
@@ -23,18 +26,21 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
-      <body className={font.className}>        
+      <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
-          <RentModal />
-          <LoginModal />  
+          <LoginModal />
           <RegisterModal />
-          <Navbar currentUser = {currentUser} />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
-        </body>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
