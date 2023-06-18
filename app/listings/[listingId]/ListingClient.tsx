d843c4d759632265ddaecd27726cdf44c7abc485
TienDuc <tienducnguyen174@gmail.com>
2023-06-18 20:44:09 +0700

    feat: fixes structure

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -6,10 +6,8 @@ import { toast } from "react-hot-toast";
 import { Range } from "react-date-range";
 import { useRouter } from "next/navigation";
 import { differenceInDays, eachDayOfInterval } from 'date-fns';
-
 import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
-
 import Container from "@/app/components/Container";
 import { categories } from "@/app/components/navbar/Categories";
 import ListingHead from "@/app/components/listings/ListingHead";
@@ -75,12 +73,12 @@ const ListingClient: React.FC<ListingClientProps> = ({
             listingId: listing?.id
         })
             .then(() => {
-                toast.success('Listing reserved!');
+                toast.success('Reserve successful!');
                 setDateRange(initialDateRange);
-                router.push('/trips');
+                router.push("/trips");
             })
             .catch(() => {
-                toast.error('Something went wrong.');
+                toast.error('Something went wrong! Please try again');
             })
             .finally(() => {
                 setIsLoading(false);
